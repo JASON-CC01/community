39256e13c118f1c20a00bce2ba71c68befa533ae
jason <1035358548@qq.com>
2020-03-10 23:16:59 +0800

    my first commit

---
+++ src/main/java/com/jason/comunication/comunication/ComunicationApplication.java
@@ -0,0 +1,15 @@
+package com.jason.comunication.comunication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComunicationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComunicationApplication.class, args);
+
+
+    }
+
+}
---
+++ src/test/java/com/jason/comunication/comunication/ComunicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jason.comunication.comunication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComunicationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
